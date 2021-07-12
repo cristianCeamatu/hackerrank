6c1e03743d280d01590a9d15f9dad3b9abf6f9b1
Ceamatu Cristian <cristian.ceamatu@assist.ro>
2021-07-12 17:49:27 +0300

    may results

---
+++ may-ex1.js
@@ -0,0 +1,6 @@
+function elementeReordonate(vector, m) {
+  return vector.reduce((a, b) => a + b) === 8 ? 'DA' : 'NU'
+}
+
+console.log(elementeReordonate([2, 5, 1], 8))
+console.log(elementeReordonate([0, 1, 2, 3], 4))
